6bab2feb0a9db5c16a99b48a39607af2c7afea47
johnpiapian <johnpiapian@gmail.com>
2020-08-01 15:45:39 -0500

    Debug controller class

--- src/extracredit/Controller.java
+++ src/extracredit/Controller.java
@@ -16,30 +16,25 @@ public class Controller {
         int[] computer = model.getMostCommontKind(model.getComputerScore());
         int[] player = model.getMostCommontKind(model.getPlayerScore());
         
-        if(computer[1] > player[1]){
+        
+        // Determine winner 1) by repeated times 2) if tie by value
+        if(computer[0] > player[0]){ // by no of times repeatead
             view.computerWins(computer);
-        }else if(computer[1] < player[1]){
+        }else if(player[0] > computer[0]){
             view.playerWins(player);
         }else{
-            if(computer[0] > player[0]){
+            if(computer[1] > player[1]){ // by value of the kind
                 view.computerWins(computer);
-            }else if(player[0] > computer[0]){
+            }else if(player[1] > computer[1]){
                 view.playerWins(player);
             }else{
-                view.tie();
+               view.tie(); 
             }
         }
         
-        System.out.println("Computer: " + computer[0] + ":" + computer[1]);
-        System.out.println("Player: " + player[0] + ":" + player[1]);
-        
-        
-        //System.out.println(Arrays.toString(model.getComputerScore()));
-        //System.out.println(Arrays.toString(model.getPlayerScore()));
-
-        //System.out.println(Arrays.toString(computer));
-        //System.out.println(Arrays.toString(player));
-        
+        // Important for debugging
+        //System.out.println("Computer: " + computer[0] + ":" + computer[1]);
+        //System.out.println("Player: " + player[0] + ":" + player[1]);
     }
 
 }
